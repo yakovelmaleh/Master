7ad803aca24cad5c6a75ef4bc25aac6a6454f97d
Yakov Elmaleh <t-yelmaleh@microsoft.com>
2023-05-05 20:08:27 +0300

    v1

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import pandas as pd
 import RunModels.select_length_doc_vector as select
 
 if __name__ == '__main__':
-    print('hello World')
+    print('start Apache')
     select.start('Apache')
+    print('finish Apache')
