b9a71fb7508770939dfcabe95293bccdfe0cb73b
Yakov Elmaleh <t-yelmaleh@microsoft.com>
2023-05-05 16:43:32 +0300

    a

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import Utils.clean_text as clean_text0
+import pandas as pd
 import RunModels.select_num_topic_model as select
 
 if __name__ == '__main__':
     print('hello World')
-    select.test()
+    data = pd.read_csv('Data/Apache/features_labels_table_os.csv')
+    data.to_csv('Models/topic_model/a.csv')
