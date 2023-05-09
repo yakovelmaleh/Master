4f5f1e8d906e34fb1f9873f4116b0f0af6fa3222
Yakov Elmaleh <t-yelmaleh@microsoft.com>
2023-05-09 17:53:22 +0300

    v1

--- Utils/ml_algorithms_optimization.py
+++ Utils/ml_algorithms_optimization.py
@@ -364,108 +364,106 @@ def run_model_optimization(x_train, x_test, y_train, y_test, project_key, label,
                                        'area_under_pre_recall_curve_rf', 'avg_precision_rf',
                                        'area_under_roc_curve_rf', 'y_pred_rf', 'num_trees', 'max_features',
                                        'max_depth', 'min_samples_split', 'min_samples_leaf', 'bootstrap'])
-    # Number of trees in random forest
-    n_estimators = [int(x) for x in np.linspace(start=200, stop=2000, num=10)]
-    # Number of features to consider at every split
-    max_features = ['auto', 'sqrt']
-    # Maximum number of levels in tree
-    max_depths = [int(x) for x in np.linspace(10, 110, num=11)]
-    max_depths.append(None)
-    # Minimum number of samples required to split a node
-    min_samples_split = [2, 5, 10]
-    # Minimum number of samples required at each leaf node
-    min_samples_leaf = [1, 2, 4]
-    # Method of selecting samples for training each tree
-    bootstraps = [True, False]
-    for num_trees in n_estimators:
-        for max_feature in max_features:
-            for max_depth in max_depths:
-                for min_sample_split in min_samples_split:
-                    for min_sample_leaf in min_samples_leaf:
-                        for bootstrap in bootstraps:
-                            accuracy_rf, confusion_matrix_rf, classification_report_rf, \
-                                area_under_pre_recall_curve_rf, avg_pre_rf, avg_auc_rf, y_pred_rf,\
-                                feature_importance = run_random_forest(x_train, x_test, y_train,
-                                                                        y_test, num_trees, max_feature, max_depth,
-                                                                   min_sample_split, min_sample_leaf, bootstrap, project_key=project_key, label=label, all_but_one_group=all_but_one_group)
-                            d = {'project_key': project_key, 'usability_label': label, 'features': features,
-                                 'feature_importance': feature_importance, 'accuracy_rf': accuracy_rf,
-                                 'confusion_matrix_rf': confusion_matrix_rf,
-                                 'classification_report_rf': classification_report_rf,
-                                 'area_under_pre_recall_curve_rf': area_under_pre_recall_curve_rf,
-                                 'avg_precision_rf': avg_pre_rf, 'area_under_roc_curve_rf': avg_auc_rf,
-                                 'y_pred_rf': y_pred_rf, 'num_trees': num_trees, 'max_features': max_feature,
-                                 'max_depth': max_depth, 'min_samples_split': min_sample_split,
-                                 'min_samples_leaf': min_sample_leaf, 'bootstrap': bootstrap}
+    parmas = {
+        'n_estimators': [int(x) for x in np.linspace(start=200, stop=2000, num=10)],
+        'max_features': ['auto', 'sqrt'],
+        'max_depth': [int(x) for x in np.linspace(10, 110, num=11)],
+        'min_samples_leaf': [1, 2, 4],
+        'min_samples_split': [2, 5, 10],
+        'bootstrap': [True, False]
+    }
 
-                            rf_results = pd.concat([rf_results, pd.DataFrame([d.values()], columns=d.keys())],
-                                                ignore_index=True)
-                            #rf_results = rf_results.append(d, ignore_index=True)
+    accuracy_rf, confusion_matrix_rf, classification_report_rf, \
+        area_under_pre_recall_curve_rf, avg_pre_rf, avg_auc_rf, y_pred_rf, \
+        feature_importance, best_params = run_best_params_CV(model=RandomForestClassifier(),dict=parmas, model_name="RF",
+                                                             x_train=x_train, x_test=x_test,y_train=y_train,
+                                                             y_test=y_test,project_key=project_key,label=label,
+                                                             all_but_one_group=all_but_one_group)
+
+    d = {'project_key': project_key, 'usability_label': label, 'features': features,
+         'feature_importance': feature_importance, 'accuracy_rf': accuracy_rf,
+         'confusion_matrix_rf': confusion_matrix_rf,
+         'classification_report_rf': classification_report_rf,
+         'area_under_pre_recall_curve_rf': area_under_pre_recall_curve_rf,
+         'avg_precision_rf': avg_pre_rf, 'area_under_roc_curve_rf': avg_auc_rf,
+         'y_pred_rf': y_pred_rf, 'num_trees': best_params['n_estimators'], 'max_features': best_params['max_features'],
+         'max_depth': best_params['max_depth'], 'min_samples_split': best_params['min_samples_split'],
+         'min_samples_leaf': best_params['min_samples_leaf'], 'bootstrap': best_params['bootstrap']}
+
+    rf_results = pd.concat([rf_results, pd.DataFrame([d.values()], columns=d.keys())],
+                           ignore_index=True)
+
+
+    path = addPath(f'Master/Models/optimization_results/{project_key}')
 
     if all_but_one_group:
-        path = addPath(f'Master/Models/optimization_results/{project_key}')
         rf_results.to_csv(
             f'{path}/results_groups_{project_key}_label_{label}_RF.csv', index=False)
     else:
-        path = addPath(f'Master/Models/optimization_results/{project_key}')
         rf_results.to_csv(
             f'{path}/optimization_results/results_{project_key}_label_{label}_RF.csv', index=False)
+
 
     # XGboost:
     xgboost_results = pd.DataFrame(columns=['project_key', 'usability_label', 'accuracy_xgboost',
                                             'confusion_matrix_xgboost', 'classification_report_xgboost',
                                             'area_under_pre_recall_curve_xgboost', 'avg_precision_xgboost',
                                             'area_under_roc_curve_xgboost', 'y_pred_xgboost', 'num_trees',
-                                            'max_features', 'max_depth', 'min_samples_split', 'min_samples_leaf'
+                                            'max_features', 'max_depth', 'min_samples_split', 'min_samples_leaf',
+                                            'scale_pos_weight', 'min_child_weight'
                                             ])
-    # Number of trees in random forest
-    n_estimators = [int(x) for x in np.linspace(start=200, stop=2000, num=10)]
-    # Number of features to consider at every split
-    max_features = ['auto', 'sqrt']
-    # Maximum number of levels in tree
-    max_depths = [int(x) for x in np.linspace(10, 110, num=11)]
-    max_depths.append(None)
-    # Minimum number of samples required to split a node
-    min_samples_split = [2, 5, 10]
-    # Minimum number of samples required at each leaf node
-    min_samples_leaf = [1, 2, 4]
-    for num_trees in n_estimators:
-        for max_feature in max_features:
-            for max_depth in max_depths:
-                for min_sample_split in min_samples_split:
-                    for min_sample_leaf in min_samples_leaf:
-                        accuracy_xgboost, confusion_matrix_xgboost, classification_report_xgboost, \
-                            area_under_pre_recall_curve_xgboost, avg_pre_xgboost, avg_auc_xgboost, \
-                            y_pred_xgboost, = run_xgboost(x_train, x_test, y_train, y_test, num_trees, max_feature, max_depth,
-                                                               min_sample_split, min_sample_leaf,project_key=project_key, label=label,all_but_one_group=all_but_one_group)
-                        d = {'project_key': project_key, 'usability_label': label,
-                             'accuracy_xgboost': accuracy_xgboost,
-                             'confusion_matrix_xgboost': confusion_matrix_xgboost,
-                             'classification_report_xgboost': classification_report_xgboost,
-                             'area_under_pre_recall_curve_xgboost': area_under_pre_recall_curve_xgboost,
-                             'avg_precision_xgboost': avg_pre_xgboost, 'area_under_roc_curve_xgboost': avg_auc_xgboost,
-                             'y_pred_xgboost': y_pred_xgboost, 'num_trees': num_trees, 'max_features': max_feature,
-                             'max_depth': max_depth, 'min_samples_split': min_sample_split,
-                             'min_samples_leaf': min_sample_leaf}
+    parmas ={
+        'n_estimators': [int(x) for x in np.linspace(start=200, stop=2000, num=10)],
+        'max_features': ['auto', 'sqrt'],
+        'max_depth': [int(x) for x in np.linspace(10, 110, num=11)],
+        'min_samples_leaf': [1, 2, 4],
+        'min_samples_split': [2, 5, 10],
+        'scale_pos_weight': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                             y_train[y_train == 0].count() / float(y_train[y_train == 1].count())],
+        'min_child_weight': range(1, 6, 2),
+    }
 
-                        xgboost_results = pd.concat([xgboost_results, pd.DataFrame([d.values()], columns=d.keys())],
-                                               ignore_index=True)
-                        #xgboost_results = xgboost_results.append(d, ignore_index=True)
+    accuracy_xgboost, confusion_matrix_xgboost, classification_report_xgboost, \
+        area_under_pre_recall_curve_xgboost, avg_pre_xgboost, avg_auc_xgboost, \
+        y_pred_xgboost, feature_importance, best_params = run_best_params_CV(model=GradientBoostingClassifier(),
+                                                                             dict=parmas,model_name='XGboost',
+                                                                             x_train, x_test, y_train, y_test,
+                                                                             project_key=project_key, label=label,
+                                                                             all_but_one_group=all_but_one_group)
+
+    d = {'project_key': project_key, 'usability_label': label,
+         'accuracy_xgboost': accuracy_xgboost,
+         'confusion_matrix_xgboost': confusion_matrix_xgboost,
+         'classification_report_xgboost': classification_report_xgboost,
+         'area_under_pre_recall_curve_xgboost': area_under_pre_recall_curve_xgboost,
+         'avg_precision_xgboost': avg_pre_xgboost, 'area_under_roc_curve_xgboost': avg_auc_xgboost,
+         'y_pred_xgboost': y_pred_xgboost, 'num_trees': best_params['n_estimators'],
+         'max_features': best_params['max_features'],
+         'max_depth': best_params['max_depth'], 'min_samples_split': best_params['min_samples_split'],
+         'min_samples_leaf': best_params['min_samples_leaf'], 'scale_pos_weight': best_params['scale_pos_weight'],
+         'min_child_weight': best_params['min_child_weight']
+         }
+
+    rf_results = pd.concat([rf_results, pd.DataFrame([d.values()], columns=d.keys())],
+                           ignore_index=True)
+
+    path = addPath(f'Master/Models/optimization_results/{project_key}')
 
     if all_but_one_group:
-        path = addPath(f'Master/Models/optimization_results/{project_key}')
         xgboost_results.to_csv(
             f'{path}/results_groups_{project_key}_label_{label}_XGboost.csv', index=False)
     else:
         xgboost_results.to_csv(
             f'{path}/results_{project_key}_label_{label}_XGboost.csv', index=False)
 
+
     # NN:
     nn_results = pd.DataFrame(columns=['project_key', 'usability_label', 'accuracy_nn',
                                        'confusion_matrix_nn', 'classification_report_nn',
                                        'area_under_pre_recall_curve_nn', 'avg_precision_nn',
                                        'area_under_roc_curve_nn', 'y_pred_nn', 'num_units_hidden_layer',
-                                       'max_iterations', 'solver', 'num_batches_size', 'activation'])
+                                       'max_iterations', 'solver', 'num_batches_size', 'activation','alpha',
+                                       'learning_rate'])
     x_train_nn = x_train
     x_test_nn = x_test
     names = list(x_train_nn.columns.values)
@@ -475,45 +473,74 @@ def run_model_optimization(x_train, x_test, y_train, y_test, project_key, label,
     scaler.fit(x_train_nn)
     x_train_nn[names] = scaler.transform(x_train_nn[names])
     x_test_nn[names] = scaler.transform(x_test_nn[names])
-    print("features:")
-    print(x_train_nn.columns.values)
-    # hidden
-    num_units_hidden_layer = [(10,), (20,), (30,), (50,), (70,), (100,), (50, 50, 50), (5, 20, 30), (30, 70, 30),
-                              (100, 50, 100)]
-    # Number of max iterations
-    max_iterations = [200, 400, 600, 800, 1000, 1500, 2000]
-    # solvers
-    solvers = ['adam', 'sgd', 'lbfgs']
-    num_batches_size = ['auto', 20, 30, 40, 50, 100]
-    activations = ['logistic', 'tanh', 'relu']
-    for num_unit_hidden_layer in num_units_hidden_layer:
-        for max_iteration in max_iterations:
-            for solver in solvers:
-                for num_batch in num_batches_size:
-                    for activation in activations:
-                        accuracy_nn, confusion_matrix_nn, classification_report_nn, \
-                            area_under_pre_recall_curve_nn, avg_pre_nn, avg_auc_nn, \
-                            y_pred_nn = run_neural_net(x_train_nn, x_test_nn, y_train,
-                                                       y_test, num_unit_hidden_layer, max_iteration,
-                                                       solver, num_batch, activation, project_key=project_key, label=label, all_but_one_group=all_but_one_group)
-                        d = {'project_key': project_key, 'usability_label': label,
-                             'accuracy_nn': accuracy_nn, 'confusion_matrix_nn': confusion_matrix_nn,
-                             'classification_report_nn': classification_report_nn,
-                             'area_under_pre_recall_curve_nn': area_under_pre_recall_curve_nn,
-                             'avg_precision_nn': avg_pre_nn, 'area_under_roc_curve_nn': avg_auc_nn,
-                             'y_pred_nn': y_pred_nn, 'num_units_hidden_layer': num_unit_hidden_layer,
-                             'max_iterations': max_iteration, 'solver': solver, 'num_batches_size': num_batch,
-                             'activation': activation}
 
-                        nn_results = pd.concat([nn_results, pd.DataFrame([d.values()], columns=d.keys())],
-                                               ignore_index=True)
-                        #nn_results = nn_results.append(d, ignore_index=True)
+    parmas ={
+        'solver': ['adam', 'sgd', 'lbfgs'],
+        'hidden_layer_sizes': [(10,), (20,), (30,), (50,), (70,), (100,), (50, 50, 50), (5, 20, 30), (30, 70, 30),
+                              (100, 50, 100)],
+        'random_state': [1],
+        'max_iter': [200, 400, 600, 800, 1000, 1500, 2000],
+        'batch_size': ['auto', 20, 30, 40, 50, 100],
+        'activation': ['logistic', 'tanh', 'relu'],
+        'alpha': [0.0001, 0.005, 0.01, 0.05],
+        'learning_rate': ['constant', 'adaptive'],
+    }
+    accuracy_nn, confusion_matrix_nn, classification_report_nn, \
+        area_under_pre_recall_curve_nn, avg_pre_nn, avg_auc_nn, \
+        y_pred_nn, best_params = run_best_params_CV(model=,x_train_nn,dict=parmas, model_name='NN', x_test_nn, y_train,
+                                       y_test, project_key=project_key, label=label,
+                                       all_but_one_group=all_but_one_group)
 
+    d = {'project_key': project_key, 'usability_label': label,
+         'accuracy_nn': accuracy_nn, 'confusion_matrix_nn': confusion_matrix_nn,
+         'classification_report_nn': classification_report_nn,
+         'area_under_pre_recall_curve_nn': area_under_pre_recall_curve_nn,
+         'avg_precision_nn': avg_pre_nn, 'area_under_roc_curve_nn': avg_auc_nn,
+         'y_pred_nn': y_pred_nn, 'num_units_hidden_layer': best_params['hidden_layer_sizes'],
+         'max_iterations': best_params['max_iter'], 'solver': best_params['solver'], 'num_batches_size': best_params['batch_size'],
+         'activation': best_params['activation'],'alpha': best_params['alpha'], 'learning_rate': best_params['learning_rate'] }
+
+    nn_results = pd.concat([nn_results, pd.DataFrame([d.values()], columns=d.keys())],
+                           ignore_index=True)
+
+    path = addPath(f'Master/Models/optimization_results/{project_key}')
     if all_but_one_group:
-        path = addPath(f'Master/Models/optimization_results/{project_key}')
         nn_results.to_csv(
             f'{path}/results_groups_{project_key}_label_{label}_NN.csv', index=False)
     else:
-        path = addPath(f'Master/Models/optimization_results/{project_key}')
         nn_results.to_csv(
             f'{path}/results_{project_key}_label_{label}_NN.csv', index=False)
+
+
+def run_best_params_CV(model, dict, model_name, x_train, x_test, y_train, y_test, project_key,label,all_but_one_group):
+    """
+    this function predict with the best params per model
+    """
+
+    clf = RandomizedSearchCV(estimator=model, param_distributions=dict, cv=3, verbose=0, random_state=42)
+    clf.fit(x_train,y_train)
+
+    # Train the model
+    clf.fit(x_train, y_train)
+    feature_imp = pd.Series(clf.feature_importances_, index=list(x_train.columns.values)).sort_values(ascending=False)
+    print(f"feature importance {model_name}:{feature_imp}")
+    y_pred = clf.predict(x_test)
+    y_score = clf.predict_proba(x_test)
+    accuracy = metrics.accuracy_score(y_test, y_pred)
+    # Model Accuracy
+    print(f"Accuracy {model_name}:", accuracy)
+    confusion_matrix = metrics.confusion_matrix(y_test, y_pred)
+    print(f"confusion_matrix {model_name}: \n {confusion_matrix}")
+    classification_report = metrics.classification_report(y_test, y_pred)
+    print(f"classification_report {model_name}: \n {classification_report}")
+    # Create precision, recall curve
+    average_precision = metrics.average_precision_score(y_test, y_score[:, 1])
+    print(f'Average precision-recall score {model_name}: {average_precision}')
+    auc = metrics.roc_auc_score(y_test, y_score[:, 1], average='macro', sample_weight=None, max_fpr=None)
+    print(f'AUC roc {model_name}: {auc}')
+    area_under_pre_recall_curve = create_pre_rec_curve(y_test, y_score, average_precision,project_key=project_key,
+                                                       label=label,all_but_one_group=all_but_one_group, algorithm=model_name)
+    print(f'area_under_pre_recall_curve {model_name}: {area_under_pre_recall_curve}')
+
+    return [accuracy, confusion_matrix, classification_report, area_under_pre_recall_curve, average_precision, auc,
+            y_pred, feature_imp, clf.best_params_]
