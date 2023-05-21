69dde5e3aead3b0ec3418ad664fcce3e0ad4254a
Yakov Elmaleh <t-yelmaleh@microsoft.com>
2023-05-21 20:01:24 +0300

    Run other DBs

--- bert_main.py
+++ bert_main.py
@@ -1,3 +1,5 @@
+import json
+
 import BERT.run_fit_setfit_bert as run_setfit_bert
 import BERT.run_test_setfit_bert as run_test_setfit_bert
 import BERT.Classic_BERT as Classic_BERT
@@ -11,4 +13,14 @@ if __name__ == '__main__':
     """
     print('Start combine_BERT APACHE')
     CombineResults.combineResultsBert('Apache', 'Master/')
+    with open('Master/Source/jira_data_for_instability_cluster.json') as f:
+        jira_data_sources = json.load(f)
+
+    print('START ALL')
+    for jira_name, jira_obj in jira_data_sources.items():
+        print("start: ", jira_name)
+        try:
+            CombineResults.combineResultsBert(jira_name, 'Master/')
+        except Exception as e:
+            print(e)
 
