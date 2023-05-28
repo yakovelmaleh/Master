e8242d1ad6f88a3ddc821d2eaf59508cc6f2fb62
Yakov Elmaleh <t-yelmaleh@microsoft.com>
2023-05-28 20:44:37 +0300

    Run BERT 512 MariaDB

--- bert_512.py
+++ bert_512.py
@@ -5,5 +5,5 @@ import BERT.Classic_BERT_512 as Classic_BERT_512
 if __name__ == '__main__':
 
     print('Start BERT')
-    Classic_BERT_512.start('Jira', 'Master/')
+    Classic_BERT_512.start('MariaDB', 'Master/')
     print('FINISH ALL')
