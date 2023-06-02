1e971d9e1a27dcae65cd6ae4f9ee127b300bfc7a
Yakov Elmaleh <t-yelmaleh@microsoft.com>
2023-06-03 00:27:58 +0300

    Run DeBERTaV3

--- bert_main.py
+++ bert_main.py
@@ -1,5 +1,5 @@
 import json
-import NLP_Models.Electra as Electra
+import NLP_Models.DeBERTaV3 as DeBERTaV3
 
 if __name__ == '__main__':
     """
@@ -12,9 +12,9 @@ if __name__ == '__main__':
     with open('Master/Source/jira_data_for_instability_cluster.json') as f:
         jira_data_sources = json.load(f)
 
-    print('START ALL Electra')
+    print('START ALL DeBERTaV3')
     for jira_name, jira_obj in jira_data_sources.items():
         print("start: ", jira_name)
-        Electra.start(jira_name)
+        DeBERTaV3.start(jira_name)
     print('FINISH ALL')
 
