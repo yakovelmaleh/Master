d446e6ab0c2e735b082ac6bf51979481f49e439b
Yakov Elmaleh <t-yelmaleh@microsoft.com>
2023-06-09 19:19:34 +0300

    run Cross_Project_MSE_Samples

--- bert_main.py
+++ bert_main.py
@@ -1,12 +1,12 @@
 import json
-import Sample_BERT.Classic_BERT_MSE_2 as Classic_BERT_MSE_2
+import Cross_Projects.Classic_BERT_MSE_Samples as Classic_BERT_MSE_Samples
 
 if __name__ == '__main__':
     """
     run_setfit_bert.start('Apache', 'Master/')
     run_test_setfit_bert.start('Apache', 'Master/')
     Classic_BERT.start('Apache', 'Master/')
-    """
+    
     
     print('Start Classic BERT')
     with open('Master/Source/jira_data_for_instability_cluster.json') as f:
@@ -15,8 +15,9 @@ if __name__ == '__main__':
     print('START ALL Classic_BERT_MSE_2')
     for jira_name, jira_obj in jira_data_sources.items():
         print("start: ", jira_name)
-        Classic_BERT_MSE_2.start(jira_name, 'Master/')
+        Classic_BERT_MSE_Samples.start(jira_name, 'Master/')
     print('FINISH ALL')
+    """
 
-    # Classic_BERT_without_valid.start()
+    Classic_BERT_MSE_Samples.start()
 
