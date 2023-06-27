8d11ddb306a82c1c804ac384bdd3824682ef6c4d
Yakov Elmaleh <t-yelmaleh@microsoft.com>
2023-06-27 20:19:05 +0300

    run UnderSampling_BERT

--- bert_main.py
+++ bert_main.py
@@ -1,5 +1,5 @@
 import json
-import NLP_Models_Current.Run_RoBERTaV1 as run_model
+import NLP_Models_Current.Run_BERT as run_model
 
 if __name__ == '__main__':
     """
@@ -16,6 +16,6 @@ if __name__ == '__main__':
     print('START')
     for jira_name, jira_obj in jira_data_sources.items():
         print("start: ", jira_name)
-        run_model.run_over_sampling(jira_name)
+        run_model.run_under_sampling(jira_name)
     print('FINISH ALL')
 
