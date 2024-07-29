27d1d2b63316f30bcf094736275d37f2fcba1741
Yakov Master <yakovelmaleh7@gmail.com>
2024-07-29 22:55:44 +0300

    using CSV files fix SetUpData.py query base

--- Using_CSV_files/CalculateData/calculate_time_add_sprint.py
+++ Using_CSV_files/CalculateData/calculate_time_add_sprint.py
@@ -7,59 +7,69 @@ import Using_CSV_files.Load_Data_From_Jira_To_CSV.Logger as Logger
 import Using_CSV_files.FilesActivity as FilesActivity
 import Using_CSV_files.Load_Data_From_Jira_To_CSV.TableColumns as TableColumns
 import Using_CSV_files.Load_Data_From_Jira_To_CSV.Create_DB as Create_DB
+from typing import List
 
 
-def add_column_time_add_to_sprint(path_to_save, main_table, change_sprint, sprints):
+def add_column_time_add_to_sprint(path_to_save, main_object_list: List[TableColumns.MainTableOS],
+                                  changes_sprint_object_list: List[TableColumns.ChangesSprintOS],
+                                  sprints_object_list: List[TableColumns.SprintsOS]):
     '''
     function which calculate the time that the USI enter to sprint
     input: sql connection, the sql query which update the database with the results, the main table with the data, the change sprint table, the sprint table
     there is no ouptut, the function calculate the time and write it to the new column in main table
     '''
     logger = Logger.get_logger_with_path_and_name(path_to_save, "calculate_time_add_sprint")
-    main_table["time_add_to_sprint"] = None
-    for i in range(0, len(main_table)):
-        main_tableObject: TableColumns.MainTableOS = TableColumns.createMainObjectFromDataFrame(main_table.iloc[i])
+
+    for index, main_tableObject in enumerate(main_object_list):
         issue_name = main_tableObject.issue_key
 
-        change_sprint_filter_df_by_issueKey = change_sprint[change_sprint['issue_key'] == issue_name]
+        change_sprint_filter_df_by_issueKey = list(filter(lambda comment: comment.issue_key == issue_name,
+                                                          changes_sprint_object_list))
 
         # the type of change_sprint_filter_df_by_issueKey[different_time_from_creat] is floatType
-        min_val_of_different_time_from_creat = min(change_sprint_filter_df_by_issueKey['different_time_from_creat'])
+        min_val_of_different_time_from_creat = 0 if len(change_sprint_filter_df_by_issueKey) == 0 \
+            else min([change.different_time_from_creat] for change in change_sprint_filter_df_by_issueKey)
 
-        change_sprint_issue = \
-            change_sprint_filter_df_by_issueKey[change_sprint_filter_df_by_issueKey['different_time_from_creat'] ==
-                                                min_val_of_different_time_from_creat]
+        change_sprint_issue: List[TableColumns.ChangesSprintOS] = \
+            [] if min_val_of_different_time_from_creat == 0 else list(filter(lambda comment: comment.different_time_from_creat == min_val_of_different_time_from_creat, change_sprint_filter_df_by_issueKey))
 
-        sprint_issue = sprints[sprints['issue_key'] == issue_name]
+        sprint_issue: List[TableColumns.SprintsOS] = (
+            list(filter(lambda sprint: sprint.issue_key == issue_name, sprints_object_list)))
 
-        if len(sprint_issue) > 0 and sprint_issue['start_date'][0] is not None:
-            time_add_to_sprint = (
-                datetime.datetime.strptime(str(sprint_issue['start_date'][i]), '%Y-%m-%d %H:%M:%S'))
+        if len(sprint_issue) > 0 and sprint_issue[0].start_date is not None:
+            time_add_to_sprint = sprint_issue[0].start_date
 
-        elif len(change_sprint_issue) > 0 and change_sprint_issue['created'][0]:
-            time_add_to_sprint = (
-                datetime.datetime.strptime(str(change_sprint_issue['created'][i]), '%Y-%m-%d %H:%M:%S'))
+        elif len(change_sprint_issue) > 0 and change_sprint_issue[0].created:
+            time_add_to_sprint = change_sprint_issue[0].created
 
         else:
-            time_add_to_sprint = datetime.datetime.strptime(str(main_tableObject.created), '%Y-%m-%d %H:%M:%S')
+            time_add_to_sprint = main_tableObject.created
 
-        main_table.at[i, 'time_add_to_sprint'] = time_add_to_sprint
+        main_tableObject.time_add_to_sprint = time_add_to_sprint
 
         FilesActivity.insert_element(path_to_save, TableColumns.MainTableOS, main_tableObject, logger)
 
-        logger.debug(f"#{i} IssueKey: {issue_name} updated time_add_to_sprint to {time_add_to_sprint}")
-        if i % 100 == 0:
-            logger.info(f'project: {main_tableObject.project_key} updated {i} USIs')
+        if index % 100 == 0:
+            logger.info(f'project: {main_tableObject.project_key} updated {index} USIs')
 
 
 def start(path_to_load, path_to_save):
     Create_DB.create_DB(path_to_save)
 
     main_data = pd.read_csv(os.path.join(path_to_load, 'main_table_os.csv'))
-    changes_sprint = pd.read_csv(os.path.join(path_to_load, 'changes_sprint_os.csv'))
-    sprints = pd.read_csv(os.path.join(path_to_load, 'sprints_os.csv'))
+    main_object_list: List[TableColumns.MainTableOS] = \
+        [TableColumns.createMainObjectFromDataFrame(row) for index, row in main_data.iterrows()]
 
-    add_column_time_add_to_sprint(path_to_save, main_data, changes_sprint, sprints)
+    changes_sprint = pd.read_csv(os.path.join(path_to_load, 'changes_sprint_os.csv'))
+    changes_sprint_object_list: List[TableColumns.ChangesSprintOS] = \
+        [TableColumns.createChangesSprintOSObjectFromDataFrame(row) for index, row in changes_sprint.iterrows()]
+
+    sprints = pd.read_csv(os.path.join(path_to_load, 'sprints_os.csv'))
+    sprints_object_list: List[TableColumns.SprintsOS] = \
+        [TableColumns.createSprintsOSObjectFromDataFrame(row) for index, row in sprints.iterrows()]
+
+    add_column_time_add_to_sprint(path_to_save, main_object_list, changes_sprint_object_list,
+                                  sprints_object_list)
 
     FilesActivity.copy_files_with_black_list(path_to_load, path_to_save, [TableColumns.MainTableOS])
 
