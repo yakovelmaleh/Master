d0a9932bcbec84f7f2523230be4d9ace2e4c3026
Yakov Master <yakovelmaleh7@gmail.com>
2025-02-20 00:40:02 +0200

    Fix bugs of refactoring from 9/12/24

--- Using_CSV_files/Load_Data_From_Jira_To_CSV/ChooseFeaturesColumns.py
+++ Using_CSV_files/Load_Data_From_Jira_To_CSV/ChooseFeaturesColumns.py
@@ -1,43 +1,70 @@
 import pandas as pd
 
 
+def get_int_values_by_less_then(df: pd.DataFrame, first_column_name: str, second_column_name: str):
+    if df.empty:
+        return pd.Series(dtype=int)
+
+    return (df[first_column_name].notna() & (df[first_column_name] < df[second_column_name])
+            ).astype(int)
+
+
+def get_int_values_by_delta(df: pd.DataFrame, first_column_name: str, second_column_name: str):
+    if df.empty:
+        return pd.Series(dtype=int)
+
+    return (df[first_column_name] - df[second_column_name] > 0).astype(int)
+
+
+def get_int_instability_by_threshold(df: pd.DataFrame, threshold: int):
+    if df.empty:
+        return pd.Series(dtype=int)
+
+    return (
+            df['num_different_words_all_text_sprint'] >= threshold &
+            df['num_changes_summary_description_acceptance_sprint'] > 0).astype(int)
+
+
 def get_information_from_main(df: pd.DataFrame) -> pd.DataFrame:
-    df['has_date_of_first_response_before_add_to_sprint'] = int(df['date_of_first_response'] is not None and
-                                                                df['date_of_first_response'] < df['time_add_to_sprint'])
-    df['has_last_updated_before_add_to_sprint'] = int(df['last_updated'] is not None and
-                                                      df['last_updated'] < df['time_add_to_sprint'])
-    df['has_resolution_date'] = int(df['resolution_date'] is not None
-                                    and df['resolution_date'] < df['time_add_to_sprint'])
+
+    df['has_date_of_first_response_before_add_to_sprint'] = (
+        get_int_values_by_less_then(df, 'date_of_first_response', 'time_add_to_sprint'))
+    df['has_last_updated_before_add_to_sprint'] = (
+        get_int_values_by_less_then(df, 'last_updated', 'time_add_to_sprint'))
+    df['has_resolution_date'] = (
+        get_int_values_by_less_then(df, 'resolution_date', 'time_add_to_sprint'))
+    df['time_status_close_before_sprint'] = (
+        get_int_values_by_less_then(df, 'time_status_close', 'time_add_to_sprint'))
+
+    df['has_changes_summary_before_sprint'] = (
+        get_int_values_by_delta(df, 'num_changes_summary_new', 'num_changes_summary_new_sprint'))
+    df['has_changes_description_before_sprint'] = (
+        get_int_values_by_delta(df, 'num_changes_summary_new', 'num_changes_summary_new_sprint'))
+    df['has_changes_acceptance_criteria_before_sprint'] = (
+        get_int_values_by_delta(df, 'num_changes_acceptance_criteria_new', 'num_changes_acceptance_criteria_new_sprint'))
+    df['has_changes_story_points_before_sprint'] = (
+        get_int_values_by_delta(df, 'num_changes_story_points_new', 'num_changes_story_points_new_sprint'))
+
     df['num_changes_summary_before_sprint'] = df['num_changes_summary_new'] - df['num_changes_summary_new_sprint']
-    df['has_changes_summary_before_sprint'] = int(df['num_changes_summary_new'] - df['num_changes_summary_new_sprint'] > 0)
     df['num_changes_description_before_sprint'] = (df['num_changes_description_new'] -
                                                    df['num_changes_description_new_sprint'])
-    df['has_changes_description_before_sprint'] = (
-        int(df['num_changes_description_new'] - df['num_changes_description_new_sprint'] > 0))
     df['num_changes_acceptance_criteria_before_sprint'] = (df['num_changes_acceptance_criteria_new'] -
                                                            df['num_changes_acceptance_criteria_new_sprint'])
-    df['has_changes_acceptance_criteria_before_sprint'] = int(df['num_changes_acceptance_criteria_new'] -
-                                                              df['num_changes_acceptance_criteria_new_sprint'] > 0)
     df['num_changes_story_points_before_sprint'] = (df['num_changes_story_points_new'] -
                                                     df['num_changes_story_points_new_sprint'])
-    df['has_changes_story_points_before_sprint'] = int(df['num_changes_story_points_new'] -
-                                                       df['num_changes_story_points_new_sprint'] > 0)
-    df['time_status_close_before_sprint'] = int(df['time_status_close'] is not None and
-                                                df['time_status_close'] < df['time_add_to_sprint'])
 
     #instability
-    df['is_change_text_num_words_1'] = int(df['num_different_words_all_text_sprint'] > 0 and
-                                           df['num_changes_summary_description_acceptance_sprint'] > 0)
-    df['is_change_text_num_words_5'] = int(df['num_different_words_all_text_sprint'] >= 5 and
-                                           df['num_changes_summary_description_acceptance_sprint'] > 0)
-    df['is_change_text_num_words_10'] = int(df['num_different_words_all_text_sprint'] >= 10 and
-                                            df['num_changes_summary_description_acceptance_sprint'] > 0)
-    df['is_change_text_num_words_15'] = int(df['num_different_words_all_text_sprint'] >= 15 and
-                                            df['num_changes_summary_description_acceptance_sprint'] > 0)
-    df['is_change_text_num_words_20'] = int(df['num_different_words_all_text_sprint'] >= 20 and
-                                            df['num_changes_summary_description_acceptance_sprint'] > 0)
-    df['is_change_text_sp_sprint'] = int(df['is_change_text_num_words_5'] > 0 and
-                                         (df['num_changes_story_points_new_sprint'] > 0 or df['num_sprints'] > 1))
+    df['is_change_text_num_words_1'] = get_int_instability_by_threshold(df, 1)
+    df['is_change_text_num_words_5'] = get_int_instability_by_threshold(df, 5)
+    df['is_change_text_num_words_10'] = get_int_instability_by_threshold(df, 10)
+    df['is_change_text_num_words_15'] = get_int_instability_by_threshold(df, 15)
+    df['is_change_text_num_words_20'] = get_int_instability_by_threshold(df, 20)
+
+    df['is_change_text_sp_sprint'] = (
+            df['is_change_text_num_words_5'] > 0 &
+            (df['num_changes_story_points_new_sprint'] > 0 | df['num_sprints'] > 1)
+    ).astype(int)
+
     df['time_until_add_to_sprint'] = (df['time_add_to_sprint'] - df['created']).total_seconds() / 60
 
     main_list = ['issue_key', 'issue_id', 'project_key', 'created', 'creator', 'reporter', 'assignee', 'priority',
